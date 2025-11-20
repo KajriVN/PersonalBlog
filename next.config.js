01eeaf935f0a48eb9bd1a79e696a2c2ebbae693d
Katjri <kajrivn@gmail.com>
2025-11-20 20:39:58 +0700

    Deployyyypls

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
